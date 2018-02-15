0384b0931c174c332628d6636777b182777309be
jjaneto <jjaneto@dcomp.ufs.br>
2018-02-15 14:26:33 -0300

    Preparing for new branch

--- 2IS/ordinary_formulation_c++.cpp
+++ 2IS/ordinary_formulation_c++.cpp
@@ -22,7 +22,7 @@ void readGraph(char *name) {
   memset(pathInstance, 0, sizeof pathInstance);
   strcat(pathInstance, "./instancias/");
   strcat(pathInstance, name);
-  printf("%s\n", pathInstance);
+  //printf("%s\n", pathInstance);
   FILE *instanceFile = fopen(pathInstance, "r");
 
   if (instanceFile == NULL) {
@@ -31,19 +31,31 @@ void readGraph(char *name) {
   }
 
   char s, t[30];
-  int x = fscanf(instanceFile, "%c %s", &s, t);
+  fscanf(instanceFile, "%c %s", &s, t);
 
   fscanf(instanceFile, "%d %d", &n, &m);
-  adj.assign(n + 1, vi());
+  adj.assign(n, vi());
   
   for (int i = 0; i < m; i += 1) {
     int u, v;
     fscanf(instanceFile, "%d %d", &u, &v);
+    u--, v--;
     adj[u].push_back(v);
     adj[v].push_back(u);
   }
 
   fclose(instanceFile);
+}
+
+void constructAdjMatrix(int *adjMatrix, int size) {
+  memset(adjMatrix, 0, size);
+  for (int i = 0; i < (int) adj.size(); i++) {
+    for (int j = 0; j < (int) adj[i].size(); j++) {
+      int v = adj[i][j];
+      adjMatrix[(i * n) + v] = 1;
+      printf("There is an edge (%d, %d), so [%d] = 1\n", i, v, (i * n) + v);
+    }
+  }
 }
 
 int main(int argc, char **argv) {
@@ -58,27 +70,33 @@ int main(int argc, char **argv) {
     GRBEnv env = GRBEnv();
     GRBModel model = GRBModel(env);
 
-    int adjMatrix[n][m];
-
+    int adjMatrix[n * m];
+    constructAdjMatrix(adjMatrix, n * m);
+    
     GRBVar vars[n * m];
     for (int idx = 0; idx < n * m; idx += 1) {
-      string varName = "var" + idx;
-      vars[idx] = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, varName);
+      ostringstream vname;
+      vname << "var" << idx;
+      vars[idx] = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, vname.str());
     }
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
-        if (adjMatrix[i][j] == 1) {
-          //GRBLineExpr constr = vars[i] + vars[j] <= 1;
-          string constrName = "constr" + i + "_" + j;
-          model.addConstr(vars[i] + vars[j] <= 1, constrName);
+        if (adjMatrix[(i * n) + j] == 1) {
+          printf("Adding constraint in %d and %d [%d]\n", i, j, (i * n) + j);
+          ostringstream cname;
+          cname << "cosntr" << (i * n) + j;      
+          model.addConstr(vars[i] + vars[j] <= 1, cname.str());
         }
       }      
     }
 
+    model.set(GRB_IntAttr_ModelSense, GRB_MAXIMIZE);
     //model.setObjective(w * x, GRB_MAXIMIZE);
 
-    //model.optimize();
+    model.optimize();
+
+    printf("End of the code.\n");
   } catch (GRBException ex) {
     cout << "Error code = " << ex.getErrorCode() << endl;
     cout << ex.getMessage() << endl;
