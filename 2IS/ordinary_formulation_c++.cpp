817e2408f10fca983f7b904a6c2dd4e9e505b821
jjaneto <jjaneto@dcomp.ufs.br>
2018-02-15 18:38:42 -0300

    Returning to adjacency list

--- 2IS/ordinary_formulation_c++.cpp
+++ 2IS/ordinary_formulation_c++.cpp
@@ -13,7 +13,48 @@ using namespace std;
 typedef vector<int> vi;
 
 char instanceName[50];
+vector<vi> adj;
 int n, m;
+
+void readGraph(char *name) {
+  strcat(instanceName, name);
+  char pathInstance[50];
+  memset(pathInstance, 0, sizeof pathInstance);
+  strcat(pathInstance, "./instancias/");
+  strcat(pathInstance, name);
+  //printf("%s\n", pathInstance);
+  FILE *instanceFile = fopen(pathInstance, "r");
+
+  if (instanceFile == NULL) {
+    printf("File cannot be open! Ending the program...\n");
+    exit(-1);
+  }
+
+  char s, t[30];
+  fscanf(instanceFile, "%c %s", &s, t);
+  fscanf(instanceFile, "%d %d", &n, &m);
+  printf("There is %d vertex and %d edges. The array have size of %d\n", n, m, (n * m));
+
+  adj.assign(n, vi());
+
+  for (int i = 0; i < m; i += 1) {
+    int u, v;
+    fscanf(instanceFile, "%d %d", &u, &v);
+    printf("edge (%d, %d)\n", u - 1, v - 1);
+    u--, v--;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+
+  
+
+  fclose(instanceFile);
+}
+
+/*GRBModel optimize() {
+
+  return nullptr;
+  }*/
 
 int main(int argc, char **argv) {
 
@@ -25,37 +66,10 @@ int main(int argc, char **argv) {
   try {
     GRBEnv env = GRBEnv();
     GRBModel model = GRBModel(env);
-    char *name = argv[1];
-    strcat(instanceName, name);
-    char pathInstance[50];
-    memset(pathInstance, 0, sizeof pathInstance);
-    strcat(pathInstance, "./instancias/");
-    strcat(pathInstance, name);
-    //printf("%s\n", pathInstance);
-    FILE *instanceFile = fopen(pathInstance, "r");
 
-    if (instanceFile == NULL) {
-      printf("File cannot be open! Ending the program...\n");
-      exit(-1);
-    }
+    readGraph(argv[1]);
 
-    char s, t[30];
-    fscanf(instanceFile, "%c %s", &s, t);
-    fscanf(instanceFile, "%d %d", &n, &m);
-    printf("There is %d vertex and %d edges. The array have size of %d\n", n, m, (n * m));
-    int adjMatrix[n * n];
-    memset(adjMatrix, 0, sizeof adjMatrix);
-    for (int i = 0; i < m; i += 1) {
-      int u, v;
-      fscanf(instanceFile, "%d %d", &u, &v);
-      printf("edge (%d, %d)\n", u - 1, v - 1);
-      u--, v--;
-      adjMatrix[(u * n) + v] = 1;
-      adjMatrix[(v * n) + u] = 1;
-    }
-
-    fclose(instanceFile);
-
+    //Adding the variables to the model.
     GRBVar vars[n];
     for (int idx = 0; idx < n; idx += 1) {
       ostringstream vname;
@@ -63,27 +77,27 @@ int main(int argc, char **argv) {
       vars[idx] = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, vname.str());
     }
 
+    //Adding the constraints to the model.
     for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (i != j && adjMatrix[(i * n) + j] == 1) {
-          printf("Adding constraint in %d and %d [%d]\n", i, j, (i * n) + j);
-          ostringstream cname;
-          cname << "cosntr" << (i * n) + j;      
-          model.addConstr(vars[i] + vars[j] <= 1, cname.str());
-        }
-      }      
+      for (int j = 0; j < (int) adj[i].size(); j++) {
+        int v = adj[i][j];
+        ostringstream cname;
+        cname << "constr" << (i * n) + v;
+        model.addConstr(vars[i] + vars[v] <= 1, cname.str());
+      }
     }
 
+    //Adding the objective.
     GRBLinExpr obj = 0.0;
     for (int var = 0; var < n; var++) {
       obj += vars[var];
     }
     
     model.setObjective(obj, GRB_MAXIMIZE);
-    //model.set(GRB_IntAttr_ModelSense, GRB_MAXIMIZE);
     model.optimize();
 
     int status = model.get(GRB_IntAttr_Status);
+    printf("Status: %d\n", status);
     if (status == GRB_OPTIMAL) {
       printf("Solution found is optimal!\n");
       for (int var = 0; var < n; var++) {
